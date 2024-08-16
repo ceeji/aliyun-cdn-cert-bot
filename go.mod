bd4e72206590086c67c26af8740d361685b43010
ceeji <i@ceeji.net>
2024-08-16 16:01:57 +0800

    支持阿里云oss

--- go.mod
+++ go.mod
@@ -4,4 +4,29 @@ go 1.22.6
 
 require github.com/denverdino/aliyungo v0.0.0-20230411124812-ab98a9173ace
 
-require github.com/opentracing/opentracing-go v1.2.0 // indirect
+require (
+	github.com/alibabacloud-go/alibabacloud-gateway-spi v0.0.4 // indirect
+	github.com/alibabacloud-go/cas-20200407/v2 v2.3.0 // indirect
+	github.com/alibabacloud-go/darabonba-openapi/v2 v2.0.6 // indirect
+	github.com/alibabacloud-go/debug v0.0.0-20190504072949-9472017b5c68 // indirect
+	github.com/alibabacloud-go/endpoint-util v1.1.0 // indirect
+	github.com/alibabacloud-go/openapi-util v0.1.0 // indirect
+	github.com/alibabacloud-go/tea v1.2.1 // indirect
+	github.com/alibabacloud-go/tea-utils v1.3.1 // indirect
+	github.com/alibabacloud-go/tea-utils/v2 v2.0.5 // indirect
+	github.com/alibabacloud-go/tea-xml v1.1.3 // indirect
+	github.com/aliyun/alibaba-cloud-sdk-go v1.63.0 // indirect
+	github.com/aliyun/alibabacloud-oss-go-sdk-v2 v1.0.2 // indirect
+	github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible // indirect
+	github.com/aliyun/credentials-go v1.3.1 // indirect
+	github.com/clbanning/mxj/v2 v2.5.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opentracing/opentracing-go v1.2.1-0.20220228012449-10b1cf09e00b // indirect
+	github.com/tjfoc/gmsm v1.3.2 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/time v0.4.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,15 +5,30 @@ import (
 	"os"
 	"time"
 
+	cas20200407 "github.com/alibabacloud-go/cas-20200407/v2/client"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/denverdino/aliyungo/cdn"
 )
 
 func main() {
+	// 调试专用
+	// os.Setenv("ALI_ACCESS_KEY_ID", "")
+	// os.Setenv("ALI_ACCESS_KEY_SECRET", "")
+	// os.Setenv("ALI_DOMAIN", "")
+	// os.Setenv("ALI_OSS_BUCKET", "")
+	// os.Setenv("ALI_CERT_PATH", "")
+	// os.Setenv("ALI_KEY_PATH", "")
+	// os.Setenv("ALI_OSS_ENDPOINT", "")
+	// os.Setenv("ALI_OSS_REGION", "")
+
 	var ACCESS_KEY_ID = os.Getenv("ALI_ACCESS_KEY_ID")
 	var ACCESS_KEY_SECRET = os.Getenv("ALI_ACCESS_KEY_SECRET")
 	var domain = os.Getenv("ALI_DOMAIN")
 	var certPath = os.Getenv("ALI_CERT_PATH")
 	var keyPath = os.Getenv("ALI_KEY_PATH")
+	var ossBucket = os.Getenv("ALI_OSS_BUCKET")
 	// acme.sh 前两行
 
 	var cert []byte
@@ -33,15 +48,98 @@ func main() {
 	fmt.Println("time: ", time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Println("update cert for domain: ", domain)
 	fmt.Println("certName: ", certName)
+	if ossBucket != "" {
+		fmt.Println("ossBucket: ", ossBucket)
+	}
 
-	client := cdn.NewClient(ACCESS_KEY_ID, ACCESS_KEY_SECRET)
-	res, err := client.SetDomainServerCertificate(cdn.CertificateRequest{
-		DomainName:              domain,
-		CertName:                certName,
-		ServerCertificateStatus: "on",
-		ServerCertificate:       string(cert),
-		PrivateKey:              string(key),
-	})
+	// 对于 CDN
+	if ossBucket == "" {
+		client := cdn.NewClient(ACCESS_KEY_ID, ACCESS_KEY_SECRET)
+		res, err := client.SetDomainServerCertificate(cdn.CertificateRequest{
+			DomainName:              domain,
+			CertName:                certName,
+			ServerCertificateStatus: "on",
+			ServerCertificate:       string(cert),
+			PrivateKey:              string(key),
+		})
 
-	fmt.Printf("res: %v, err: %v\n", res, err)
+		fmt.Printf("res: %v, err: %v\n", res, err)
+		return
+	}
+
+	// 对于 OSS
+	if ossBucket != "" {
+		os.Setenv("OSS_ACCESS_KEY_ID", ACCESS_KEY_ID)
+		os.Setenv("OSS_ACCESS_KEY_SECRET", ACCESS_KEY_SECRET)
+		region := os.Getenv("ALI_OSS_REGION")
+		if region == "" {
+			fmt.Println("Error: OSS_REGION is required")
+			os.Exit(-1)
+		}
+		endpoint := os.Getenv("ALI_OSS_ENDPOINT")
+
+		// 证书管理服务客户端
+		casClient, err := cas20200407.NewClient(&openapi.Config{
+			AccessKeyId:     tea.String(ACCESS_KEY_ID),
+			AccessKeySecret: tea.String(ACCESS_KEY_SECRET),
+
+			// endpoint参考：https://api.aliyun.com/product/cas
+			Endpoint: tea.String("cas.aliyuncs.com"),
+		})
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(-1)
+		}
+		req := new(cas20200407.UploadUserCertificateRequest)
+		// 证书名称
+		req.Name = tea.String(certName)
+		// 证书私钥
+		req.Key = tea.String(string(key))
+		// 证书内容
+		req.Cert = tea.String(string(cert))
+		// 上传证书到证书管理服务
+		resp, err := casClient.UploadUserCertificate(req)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(-1)
+		}
+
+		if *resp.StatusCode != 200 {
+			fmt.Println("Error:", resp.Body)
+			os.Exit(-1)
+		}
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(-1)
+		}
+
+		certId := resp.Body.CertId
+
+		// 创建OSSClient实例。
+		// yourEndpoint填写Bucket对应的Endpoint，以华东1（杭州）为例，填写为https://oss-cn-hangzhou.aliyuncs.com。其它Region请按实际情况填写。
+		client, err := oss.New(endpoint, ACCESS_KEY_ID, ACCESS_KEY_SECRET)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(-1)
+		}
+
+		var putCnameConfig oss.PutBucketCname
+		var CertificateConfig oss.CertificateConfiguration
+		// 填写自定义域名。
+		putCnameConfig.Cname = domain
+		// 填写证书ID。
+		CertificateConfig.CertId = fmt.Sprintf("%d-%s", *certId, os.Getenv("OSS_REGION"))
+		// CertificateConfig.Certificate = string(cert)
+		// CertificateConfig.PrivateKey = string(key)
+		CertificateConfig.Force = true
+		putCnameConfig.CertificateConfiguration = &CertificateConfig
+		err = client.PutBucketCnameWithCertificate(ossBucket, putCnameConfig)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(-1)
+		}
+
+		fmt.Printf("Bind Certificate Success!")
+	}
 }
